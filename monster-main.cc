51556d69999623be923e672287bea200d715ec6e
Neil Moore <neil@s-z.org>
2015-06-14 19:16:19 -0400

    Don't show rN from items.
    It was happening for Sigmund.

--- monster-main.cc
+++ monster-main.cc
@@ -1216,7 +1216,7 @@ int main(int argc, char *argv[])
 
     res2(LIGHTBLUE,    drown,  mon.res_water_drowning());
     res2(LIGHTRED,     rot,    mon.res_rotting());
-    res2(LIGHTMAGENTA, neg,    mon.res_negative_energy());
+    res2(LIGHTMAGENTA, neg,    mon.res_negative_energy(true));
     res2(YELLOW,       holy,   mon.res_holy_energy(&you));
     res2(LIGHTMAGENTA, torm,   mon.res_torment());
     res2(LIGHTBLUE,    wind,   mon.res_wind());
